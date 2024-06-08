5166145d2b3f4e6bb0c9c47a965ae439f75eebd2
Ada-Patrick <patrick@bepeerless.co>
2024-06-08 21:57:37 +0100

    Introduced a dummy api key

--- PaymentService.API/obj/Debug/net6.0/PaymentService.API.AssemblyInfo.cs
+++ PaymentService.API/obj/Debug/net6.0/PaymentService.API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaymentService.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+568b3eba5df9875e5fdee54699605e5108fbf882")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaymentService.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaymentService.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
